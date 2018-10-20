f8fe8639383f6a26b4fec6932c3b341f979af6fd
Helen Humphrey <helen@helenhumphrey.co>
2018-10-20 16:53:03 +0100

    chore(Stylelint): Add stylelint and fix current css

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'vue/html-indent': ["error", 4],
+        'vue/html-indent': ['error', 4],
+        'quotes': ['error', 'single'],
     }
 }
