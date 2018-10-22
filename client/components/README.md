5e2dee008b5938db8b66d96793472f5fb47be5c0
Helen Humphrey <helen@helenhumphrey.co>
2018-10-22 13:36:42 +0100

    chore: Move all client code to client directory

---
+++ client/components/README.md
@@ -0,0 +1,3 @@
+# COMPONENTS
+
+The components directory contains shared Vue.js Components.
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ const pkg = require('./package')
 
 module.exports = {
     mode: 'universal',
+    srcDir: 'client/',
     /*
     ** Headers of the page
     */
